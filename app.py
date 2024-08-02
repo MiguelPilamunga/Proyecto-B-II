72aaacf95b4a47b91fb18c8621e4c93516161691
Miguel Pilamunga <lpilamunga@innvcode.com>
2024-08-02 12:35:22 +0000

    feat: chequeo de anomalias en datos item 3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
+from routes.AuditRoute import auditoria
 from routes.MetodosGenericos import integridad
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:4200", "http://localhost:3000"]}})
 
 app.register_blueprint(integridad)
+app.register_blueprint(auditoria)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
